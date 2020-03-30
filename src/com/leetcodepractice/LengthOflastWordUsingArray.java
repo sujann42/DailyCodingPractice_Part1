0c5cff48aded138960d3e4f3cbaae47116483df5
suzan <sujann42@gmail.com>
2020-03-29 19:28:42 -0500

    another way of finding the length of the last word in a string....

--- src/com/leetcodepractice/LengthOflastWordUsingArray.java
+++ src/com/leetcodepractice/LengthOflastWordUsingArray.java
@@ -2,26 +2,33 @@ package com.leetcodepractice;
 
 public class LengthOflastWordUsingArray {
     public static void main(String[] args) {
-        String str1 = " a b";
-        String str2 = "a";
-        String str3 = "a ";
-        System.out.println("Sizes of: ");
-        System.out.println("str1: " + str1.length() + " " + " str2: " + str2.length() + " " + " str3: " + str3.length());
-        System.out.println(getLenOfLastWrdUsingList(str1));
-        System.out.println(getLenOfLastWrdUsingList(str2));
-        System.out.println(getLenOfLastWrdUsingList(str3));
-        String str4 = "hello";
-        System.out.println(str1.trim());
+        String str1 = " apple ball";
+        String str2 = "aeropla";
+        String str3 = "auctionss ";
+        String str4 = " ";
+
+        System.out.println("Length of last word in string is:  " + getLenOfLastWrdUsingList(str1));
+        System.out.println("Length of last word in string is:  " + getLenOfLastWrdUsingList(str2));
+        System.out.println("Length of last word in string is:  " + getLenOfLastWrdUsingList(str3));
+        System.out.println("Length of last word in string is:  " + getLenOfLastWrdUsingList(str4));
     }
 
     public static int getLenOfLastWrdUsingList(String s) {
-        String[] arr = s.split(" ");
-        System.out.println(arr.length);
+        String str = s.trim();
+        int lengthofLastWord = 0;
+        if (str.isEmpty() || str.isBlank()) {
+            return 0;
+        }
+        System.out.println("After trim:" + str);
+        String[] arr = str.split(" ");
+        System.out.println("Length of Array: " + arr.length);
+        System.out.print("Elements in array:");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        System.out.println(" ");
+        System.out.println();
+        lengthofLastWord = arr[arr.length - 1].length();
 
-        return 12354;
+        return lengthofLastWord;
     }
 }
